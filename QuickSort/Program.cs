a78a6e8ff37891ab366b4588e87a76a73c130f00
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2022-10-21 16:58:31 +0700

    Create void input

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -8,7 +8,19 @@ namespace QuickSort
 {
     class Program
     {
+        //Array of intgers to hold values
         private int[] arr = new int[20];
+        private int cmp_count = 0;//Number of comparisons
+        private int mov_count = 0;//Number of data movments
+
+        //Number of elements in array
+        private int n;
+
+
+        void input()
+        {
+
+        }
 
         static void Main(string[] args)
         {
